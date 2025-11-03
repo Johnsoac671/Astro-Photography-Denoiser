05eefceb3bd01bc0675d10231afbab395d5b4b86
Aidan Johnson <johnsoac671@gmail.com>
2025-11-02 18:24:40 -0800

    made CNN much deeper, getting decent results, now just fine tuning

--- upscaler.py
+++ upscaler.py
@@ -2,12 +2,14 @@ import os
 import cv2 as cv
 import numpy
 import astropy.io.fits as fits
+import time
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import Adam
+from torch.amp import autocast, GradScaler
 
 
 CUTOUT_DIR_SMALL = "fitssmall"
@@ -43,6 +45,19 @@ def get_fits(dir, galaxy_id):
 
     return stacked
 
+def format_time(seconds):
+    
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    secs = int(seconds % 60)
+    
+    return f"{hours:02d}:{minutes:02d}:{secs:02d}"
+
+def normalize_image(img):
+    low, high = numpy.percentile(img, [1, 99])
+    img_clipped = numpy.clip(img, low, high)
+    return (img_clipped - img_clipped.min()) / (img_clipped.max() - img_clipped.min() + 1e-8)
+
 
 def build_dataset():
     ids = get_galaxy_ids()
@@ -75,6 +90,10 @@ def build_dataset():
             
             if index % 100 == 0:
                 print(f"Done with: {index}")
+    
+    for i in range(len(low_res_fits)):
+        low_res_fits[i] = normalize_image(low_res_fits[i])
+        high_res_fits[i] = normalize_image(high_res_fits[i])
 
     low_res_data = numpy.stack(low_res_fits)
     high_res_data = numpy.stack(high_res_fits)
@@ -85,17 +104,35 @@ def build_dataset():
 class CnnUpscaler(nn.Module):
     def __init__(self, scale_factor=4):
         super().__init__()
+        
         self.conv1 = nn.Conv2d(1, 64, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(32, scale_factor**2, kernel_size=3, padding=1)
-        self.pixel_suffle = nn.PixelShuffle(scale_factor)
-        self.relu = nn.ReLU()
+        self.b1 = nn.BatchNorm2d(64)
+        
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.b2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.b3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.b4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.b5 = nn.BatchNorm2d(64)
+        
+        self.conv6 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
+        self.conv7 = nn.Conv2d(32, scale_factor**2, kernel_size=3, padding=1)
+        
+        self.pixel_shuffle = nn.PixelShuffle(scale_factor)
+        self.relu = nn.ReLU(inplace=True)
         
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.conv3(x)
-        x = self.pixel_suffle(x)
+        x = self.relu(self.b1(self.conv1(x)))
+        x = self.relu(self.b2(self.conv2(x)))
+        x = self.relu(self.b3(self.conv3(x)))
+        x = self.relu(self.b4(self.conv4(x)))
+        x = self.relu(self.b5(self.conv5(x)))
+        x = self.relu(self.conv6(x))
+        x = self.conv7(x)
+        x = self.pixel_shuffle(x)
+        
         return x
     
 
@@ -114,32 +151,59 @@ class ImageDataset(Dataset):
 
 
 if __name__ == "__main__":
-    compute_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
+    compute_device = torch.device(device_type)
+    
+    
     model = CnnUpscaler().to(compute_device)
     loss_model = nn.MSELoss()
-    optimizer_model = Adam(model.parameters())
+    optimizer_model = Adam(model.parameters(), lr=0.001)
 
     dataset = ImageDataset(*build_dataset())
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, pin_memory=True)
     
+    scaler = GradScaler()
+    
+    start = time.time()
     iterations = 50
+    
     for iteration in range(iterations):
         model.train()
         total_loss = 0
+        iteration_time = time.time()
         
         for low, high in dataloader:
             low_res = low.to(compute_device)
             high_res = high.to(compute_device)
             
-            output = model(low_res)
-            loss = loss_model(output, high_res)
-            
             optimizer_model.zero_grad()
-            loss.backward()
-            optimizer_model.step()
             
-            total_loss += loss.item()
+            with autocast(device_type):
+                output = model(low_res)
+                loss = loss_model(output, high_res)
+            
+            scaler.scale(loss).backward()
+            scaler.step(optimizer_model)
+            scaler.update()
         
-        print(f'Interation [{iteration + 1}/{iterations}], Loss: {total_loss/len(dataloader):.4f}')
+            total_loss += loss.item()
+            
+        end = time.time()
+
+        avg_time = (end - start) / (iteration + 1)
+        remaining = avg_time * (iterations - iteration - 1)
+
+        print(f'Iteration [{iteration + 1}/{iterations}], '
+            f'Loss: {total_loss/len(dataloader):.4f}, '
+            f'Iteration Time: {end - iteration_time:.2f}s, '
+            f'Total Time: {format_time(end - start)}, '
+            f'Estimated Remaining: {format_time(remaining)}')
+        
+        if (iteration + 1) % 5 == 0:
+            torch.save({'epoch': iteration,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer_model.state_dict(),
+                        'loss': total_loss/len(dataloader), },
+                       f"checkpoint_epoch_{iteration + 1}.pth")
     
     torch.save(model.state_dict(), "upscale_model.pth")
